64fc3effb06696cb6e21239615283e25b6970006
brennanpoetic <brennan@poetic.io>
2023-05-04 15:50:52 -0500

    ui updates

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import clsx from "clsx";
 import { useDropzone } from "react-dropzone";
 import Page from "@components/Page";
@@ -15,9 +15,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { faFile } from "@fortawesome/free-solid-svg-icons";
 
-
-
-
 function LandingPage() {
 	const [question, setQuestion] = React.useState("");
 	const [uploadedFiles, setUploadedFiles] = React.useState<
@@ -31,11 +28,7 @@ function LandingPage() {
 	const [loading, setLoading] = React.useState(false);
 	const [apiKey, setApiKey] = React.useState("");
 	const [apiKeyApplied, setApiKeyApplied] = React.useState(false);
-
-
-
-
-
+	const [visible, setVisible] = React.useState(false);
 
 	const handleApplyKey = () => {
 		localStorage.setItem("openai-api-key", apiKey);
@@ -120,52 +113,69 @@ function LandingPage() {
 		<Page
 			pageClass={styles.page}
 			contentBackgroundClass={styles.background}
-			contentPreferredWidth={700}
+			contentPreferredWidth={1200}
 			contentClass={styles.pageContent}
 		>
-			{/*break this into 2 slides, Get Started and Chat Interface*/}
-			
+			{/*HIDE OPENAI API KEY IN LOGO*/}
+
 			<div className={styles.text}>
-
-
-
 				<div className={styles.workArea}>
-					<div className={styles.leftColumn}>
+					<div className={styles.horizontalContainer}>
+						<div className={styles.leftColumn}>
+							<div>
+								<button
+									className={styles.btn}
+									onClick={() => setVisible(!visible)}
+								>
+									{visible ? "" : ""}
+									<h1>Get Started With PoeticAI</h1>
+								</button>
+								{visible && (
+									<>
+										<div id="apiKey">
+											<div className={styles.questionInput}>
+												<input
+													type="text"
+													value={apiKey}
+													placeholder="Enter your OpenAI API key here..."
+													onChange={(event) => setApiKey(event.target.value)}
+												/>
+												<button
+													type="submit"
+													className={styles.askQuestion}
+													onClick={handleApplyKey}
+												>
+													Apply
+												</button>
+											</div>
+										</div>
+										<div className={styles.tokenCount}>
+											To get an API key, visit the{" "}
+											<Go
+												to="https://platform.openai.com/account/api-keys"
+												data-action={""}
+												data-category={""}
+											>
+												<span
+													style={{
+														fontWeight: 900,
+														textDecoration: "underline",
+													}}
+												>
+													OpenAI API Keys Page
+												</span>
+											</Go>
+										</div>
+									</>
+								)}
+							</div>
 
+							{errorMessage && (
+								<div className={styles.error}>{errorMessage}</div>
+							)}
 
-						{!apiKeyApplied && (
 							<>
-							<h1>Get Started With PoeticAI</h1>
-							{errorMessage && <div className={styles.error}>{errorMessage}</div>}
-								<div className={styles.questionInput}>
-									<input
-										type="text"
-										value={apiKey}
-										placeholder="Enter your OpenAI API key here..."
-										onChange={(event) => setApiKey(event.target.value)}
-									/>
-									<button
-										type="submit"
-										className={styles.askQuestion}
-										onClick={handleApplyKey}
-									>
-										Apply
-									</button>
-
-								</div>
-								<div className={styles.tokenCount}>
-									To get an API key, visit the{" "}
-									<Go to="https://platform.openai.com/account/api-keys" data-action={""} data-category={""}>
-										<span
-											style={{
-												fontWeight: 900,
-												textDecoration: "underline",
-											}}
-										>
-											OpenAI API Keys Page
-										</span>
-									</Go>
-								</div>
+								{/* Get Started SECTION*/}
 								<div
 									{...getRootProps()}
 									className={clsx(
@@ -173,84 +183,94 @@ function LandingPage() {
 										loading && styles.dropzoneLoading
 									)}
 								>
-									<input {...getInputProps()} disabled={loading}  />
+									<input {...getInputProps()} disabled={loading} />
 									<p className={styles.dropzoneText}>
 										{loading
 											? "Loading..."
-											: "Drag and drop files to add to the knowledgebase here, or click to select files" }
-
+											: "Drag and drop files to add to the knowledge base here, or click to select files"}
 									</p>
-									<FontAwesomeIcon icon={faFile} className={styles.dropzoneIcon}/>
+									<FontAwesomeIcon
+										icon={faFile}
+										className={styles.dropzoneIcon}
+									/>
 								</div>
 
 								<button
-										type="submit"
-										className={styles.button}
-										onClick={handleApplyKey}
-									>
-										Next
-										<FontAwesomeIcon icon={faArrowRight} className={styles.icon} />
-									</button>
-
-									<div className={styles.chatSection}></div>
-									<div className={styles.questionInput}>
-							<input
-								type="text"
-								value={question}
-								placeholder="So, what's on your mind?"
-								onChange={(event) => setQuestion(event.target.value)}
-							/>
-							<button
-								type="submit"
-								disabled={loading || !question}
-								className={
-									loading ? styles.askQuestionDisabled : styles.askQuestion
-								}
-								onClick={handleAskQuestion}
-							>
-								Submit
-							</button>
-							{loading && <div className={styles.loader} />}
-						</div>
+									type="submit"
+									className={styles.button}
+									onClick={handleApplyKey}
+								>
+									Next
+									<FontAwesomeIcon
+										icon={faArrowRight}
+										className={styles.icon}
+									/>
+								</button>
 							</>
-						)}
-						{response?.tokens && (
-							<div className={styles.tokenCount}>
-								TOTAL TOKENS USED:{" "}
-								<span style={{ fontWeight: 900 }}>{response.tokens}</span>
+							{/* Chat SECTION*/}
+
+
+							<div className={styles.rightColumn}>
+							<>
+								<div className={styles.questionInput}>
+									<input
+										type="text"
+										value={question}
+										placeholder="So, what's on your mind?"
+										onChange={(event) => setQuestion(event.target.value)}
+									/>
+									<button
+										type="submit"
+										disabled={loading || !question}
+										className={
+											loading ? styles.askQuestionDisabled : styles.askQuestion
+										}
+										onClick={handleAskQuestion}
+									>
+										Submit
+									</button>
+									{loading && <div className={styles.loader} />}
+								</div>
+
+								{response?.tokens && (
+									<div className={styles.tokenCount}>
+										TOTAL TOKENS USED:{" "}
+										<span style={{ fontWeight: 900 }}>{response.tokens}</span>
+									</div>
+								)}
+								{response && <ResponseDisplay response={response} />}
+								<div style={{ height: 32 }} />
+								<div className={styles.fileList}>
+									{uploadedFiles.length > 0 && (
+										<div className={styles.successfulFiles}>
+											<h4>Uploaded Files:</h4>
+											<ul>
+												{uploadedFiles.map((file, index) => (
+													<li key={index}>{file}</li>
+												))}
+											</ul>
+										</div>
+									)}
+									{failedFilenames.length > 0 && (
+										<div className={styles.failedFiles}>
+											<h4>Failed Files:</h4>
+											<ul>
+												{failedFilenames.map((file, index) => (
+													<li key={index}>
+														{file}
+														<div>Reason: {failedFiles[file]}</div>
+													</li>
+												))}
+											</ul>
+										</div>
+									)}
+								</div>
+							</>
 							</div>
-						)}
-						{response && <ResponseDisplay response={response} />}
-						<div style={{ height: 32 }} />
-						<div className={styles.fileList}>
-							{uploadedFiles.length > 0 && (
-								<div className={styles.successfulFiles}>
-									<h4>Uploaded Files:</h4>
-									<ul>
-										{uploadedFiles.map((file, index) => (
-											<li key={index}>{file}</li>
-										))}
-									</ul>
-								</div>
-							)}
-							{failedFilenames.length > 0 && (
-								<div className={styles.failedFiles}>
-									<h4>Failed Files:</h4>
-									<ul>
-										{failedFilenames.map((file, index) => (
-											<li key={index}>
-												{file}
-												<div>Reason: {failedFiles[file]}</div>
-											</li>
-										))}
-									</ul>
-								</div>
-							)}
 						</div>
 					</div>
 				</div>
-				</div>
-
+			</div>
 		</Page>
 	);
 }
